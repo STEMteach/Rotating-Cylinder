dee0f4b902e9e2b004f3688037c44a0372fe0d25
Cyndy Hagin <cyndy.hagin@gmail.com>
2018-06-27 11:24:35 -0700

    rotating cylinder tutorial

---
+++ Assets/Scripts/CylinderRotateScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderRotateScript : MonoBehaviour {
+
+    //controls rotation speed
+    public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(speed, 0, 0);
+	}
+}
